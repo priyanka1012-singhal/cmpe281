247a401c55835bba598c765c8fdd4fb960b5fe3c
jainsupriya <supriya.jain@sjsu.edu>
2018-12-02 13:16:07 -0800

    import for pathvariable

--- CloudSense/src/main/java/smartstreet/controller/AppController.java
+++ CloudSense/src/main/java/smartstreet/controller/AppController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -149,5 +150,4 @@ public class AppController {
 	    return mav;
 	}
 
-    
 }
