d3236d4a9717cce096993cac987d7b07f5f0c51f
jainsupriya <supriya.jain@sjsu.edu>
2018-12-02 11:20:03 -0800

    added signin and register flow

--- CloudSense/src/main/java/smartstreet/controller/AppController.java
+++ CloudSense/src/main/java/smartstreet/controller/AppController.java
@@ -2,19 +2,31 @@ package smartstreet.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import smartstreet.dao.ISensorDao;
+import smartstreet.dao.ISmartNodeDao;
+import smartstreet.dao.impl.SensorDaoImpl;
+import smartstreet.dao.impl.SmartNodeDaoImpl;
+import smartstreet.dao.impl.UserDaoImpl;
 import smartstreet.dto.ResponseDto;
 import smartstreet.model.Cluster;
+import smartstreet.model.Login;
 import smartstreet.model.Sensor;
 import smartstreet.model.SmartNode;
+import smartstreet.model.User;
 import smartstreet.service.IClusterService;
 import smartstreet.service.ISensorService;
 import smartstreet.service.ISmartNodeService;
@@ -23,13 +35,22 @@ import smartstreet.service.ISmartNodeService;
  
 @Controller
 public class AppController {
+	private int editSensorId;
+	private int editnodeId;
+	private int editClusterId;	
 	@Autowired
 	ISensorService sensorService;
 	@Autowired
 	ISmartNodeService smartNodeService;
 	@Autowired
 	IClusterService clusterService;
-	
+	@Autowired
+	UserDaoImpl uDaoImpl = new UserDaoImpl();	
+	@Autowired
+	ISensorDao sensorDao = new SensorDaoImpl();
+	@Autowired
+	ISmartNodeDao smNodeDao = new SmartNodeDaoImpl();
+	@Autowired	
 	private final static Logger logger = Logger.getLogger(AppController.class.getName());
 	
 	@RequestMapping(value = { "/"}, method = RequestMethod.GET)
@@ -101,6 +122,32 @@ public class AppController {
         return "generatenergyusagereports";
     }
 
+    //to register user
+    @RequestMapping(value = "/adduser", method = RequestMethod.POST)
+    public ModelAndView addUser(@ModelAttribute("user") User user) throws Exception {
+    	uDaoImpl.addUser(user);
+        return new ModelAndView("signin");        
+    }
+    
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response,
+	@ModelAttribute("login") Login login) 
+	{
+		logger.info(login.getEmail());
+		logger.info(login.getUserPwd());
+		ModelAndView mav = new ModelAndView();
+		User user = uDaoImpl.validateUsers(login);
+		if (null != user) 
+		{	    	
+			mav.setViewName("sensorstationdashboard");
+		} 
+		else 
+		{
+			mav = new ModelAndView("signin");
+			mav.addObject("message", "Invalid credentials..!!! Try again...");
+		}
+	    return mav;
+	}
 
     
 }
